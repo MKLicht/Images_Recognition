a93d0c999467df107504a36902cf1e9669981978
MKLicht <67897233+MKLicht@users.noreply.github.com>
2022-08-09 12:20:58 +0900

    Update mnist.py

--- MNIST_fully/mnist.py
+++ MNIST_fully/mnist.py
@@ -43,10 +43,10 @@ class MnistNet(nn.Module):
     def __init__(self):
         super(MnistNet, self).__init__()
         self.fc1 = nn.Sequential(
-            nn.Linear(28*28, 28),
+            nn.Linear(28*28, 100),
             nn.ReLU(),
         )        
-        self.fc2 = nn.Linear(28, 10)   
+        self.fc2 = nn.Linear(100, 10)   
     def forward(self, x):
         x = x.view(-1, 28*28*1)
         x = self.fc1(x)
